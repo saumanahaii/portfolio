bbf4e9d4267e96aa1cc0ad838551d7e5291375fc
Zach Williams <saumanahaii@gmail.com>
2017-08-08 12:19:12 -0400

    initial commit

---
+++ res/js/app.js
@@ -0,0 +1,22 @@
+console.log("Working!");
+
+$(".content").addClass("hidden");
+//$(".content").velocity('transition.fadeIn');
+
+let activePage = "";
+let hash = window.location.hash;
+if (hash === "" || hash === "#about") {
+  $("#about").removeClass("hidden");
+} else if (hash === "#webdev") {
+  $("#webdev").removeClass("hidden");
+} else if (hash === "#gamedev") {
+  $("#gamedev").removeClass("hidden");
+} else if (hash === "#misc") {
+  $("#misc").removeClass("hidden");
+}
+
+$(".menu-link").click(((event) => {
+  let target = $(event.currentTarget)[0].hash;
+  $(".content").addClass("hidden");
+  $(target).removeClass("hidden");
+}))
